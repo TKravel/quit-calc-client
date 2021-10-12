ed2ba90011356d3a04e21762eb11c6e41176536a
Tim Kravel <TKravel@Yahoo.com>
2021-10-12 01:33:17 -0400

    datepicker attached to state variable

--- src/components/mainApp/MainApp.jsx
+++ src/components/mainApp/MainApp.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+	MuiPickersUtilsProvider,
+	KeyboardDatePicker,
+} from '@material-ui/pickers';
 import DateInput from './DateInput';
 
 const MainApp = () => {
+	const date = new Date();
+	const [quitDate, setQuitDate] = useState(date);
+
+	const onChange = (value) => {
+		setQuitDate(value);
+		console.log(value, quitDate);
+	};
 	return (
 		<div className='app-container'>
-			<div className='date-container'>date</div>
+			<div className='date-container'>
+				<MuiPickersUtilsProvider utils={DateFnsUtils}>
+					<KeyboardDatePicker
+						onChange={onChange}
+						value={quitDate}
+						format='MM/dd/yyyy'
+					/>
+				</MuiPickersUtilsProvider>
+			</div>
 			<div className='savings-container'>savings</div>
 			<div className='goal-container'>goals</div>
 		</div>
