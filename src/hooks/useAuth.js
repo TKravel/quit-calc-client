780275293c321610135b10586c3d1a6c444cf227
Tim Kravel <TKravel@Yahoo.com>
2021-10-28 22:13:05 -0400

    start login auth req

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,22 +2,16 @@ import { useState } from 'react';
 
 const useAuth = () => {
 	const [user, setUser] = useState('');
-	const verifyUser = () => {};
+	const getUser = () => {};
+
 	const setCookie = () => {};
 
 	const logout = () => {};
 
-	const login = (data) => {};
-
-	const register = () => {};
-
-	return {
-		verifyUser,
-		setCookie,
-		logout,
-		login,
-		register,
-	};
-};
-
-export default useAuth;
+	const login = (data) => {
+		fetch('localhost:3000', {
+			method: 'POST',
+			body: JSON.stringify(data),
+		})
+			.then((response) => response.json())
+			.then((re
